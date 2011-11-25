a842f2df503b2ddadcb2a6a5e4ee56db219078ac
mashiro <mail@mashiro.org>
2011-11-25 14:19:55 +0900

    fix

--- lib/sakuramochi.rb
+++ lib/sakuramochi.rb
@@ -2,10 +2,10 @@ require 'active_support/core_ext'
 require 'active_support'
 require 'active_record'
 require 'sakuramochi/config'
-require 'sakuramochi/condition'
-require 'sakuramochi/relation'
 require 'sakuramochi/predicate'
 require 'sakuramochi/predicate_builder'
+require 'sakuramochi/condition'
+require 'sakuramochi/relation'
 
 ActiveSupport.on_load(:active_record) do
   ActiveRecord::Relation.send(:include, Sakuramochi::Relation)
