da996317bc8b8c8c0cd27c306f9f7fcf8e895498
mashiro <mail@mashiro.org>
2012-01-10 15:19:44 +0900

    Remove deprecated InstanceMethod scope.

--- lib/sakuramochi/relation.rb
+++ lib/sakuramochi/relation.rb
@@ -10,50 +10,47 @@ module Sakuramochi
       alias :build_where :build_where_with_condition
     end
 
-    module InstanceMethods
-      def collapse_conditions(node, other)
-        case node
-        when Sakuramochi::Condition::Nodes::Expression
-          case node.operator.to_s
-          when 'and'
-            left = collapse_conditions(node.left, other)
-            right = collapse_conditions(node.right, other)
-            left && right ? Arel::Nodes::And.new([left, right]) : left || right
+    def collapse_conditions(node, other)
+      case node
+      when Sakuramochi::Condition::Nodes::Expression
+        case node.operator.to_s
+        when 'and'
+          left = collapse_conditions(node.left, other)
+          right = collapse_conditions(node.right, other)
+          left && right ? Arel::Nodes::And.new([left, right]) : left || right
 
-          when 'or'
-            left = collapse_conditions(node.left, other)
-            right = collapse_conditions(node.right, other)
-            left && right ? Arel::Nodes::Or.new(left, right) : left || right
-          end
-
-        when Sakuramochi::Condition::Nodes::Term
-          case node.operator.to_s
-          when 'not'
-            right = collapse_conditions(node.value, other)
-            Arel::Nodes::Not.new(right) if right
-          end
-
-        when Sakuramochi::Condition::Nodes::Factor
-          wheres = build_where_without_condition(node.value, other)
-          arel = table.from(table)
-          collapse_wheres(arel, (wheres - ['']).uniq)
-          arel.constraints.inject { |left, right| left.and(right) }
-
-        when Sakuramochi::Condition::Nodes::Group
-          expression = collapse_conditions(node.expression, other)
-          Arel::Nodes::Grouping.new(expression) if expression
+        when 'or'
+          left = collapse_conditions(node.left, other)
+          right = collapse_conditions(node.right, other)
+          left && right ? Arel::Nodes::Or.new(left, right) : left || right
         end
-      end
 
-      def build_where_with_condition(opts, other = [])
-        if Sakuramochi::Condition.condition?(opts)
-          ast = Sakuramochi::Condition::Parser.new(opts.dup).parse
-          [collapse_conditions(ast, other)].compact
-        else
-          build_where_without_condition(opts, other)
+      when Sakuramochi::Condition::Nodes::Term
+        case node.operator.to_s
+        when 'not'
+          right = collapse_conditions(node.value, other)
+          Arel::Nodes::Not.new(right) if right
         end
+
+      when Sakuramochi::Condition::Nodes::Factor
+        wheres = build_where_without_condition(node.value, other)
+        arel = table.from(table)
+        collapse_wheres(arel, (wheres - ['']).uniq)
+        arel.constraints.inject { |left, right| left.and(right) }
+
+      when Sakuramochi::Condition::Nodes::Group
+        expression = collapse_conditions(node.expression, other)
+        Arel::Nodes::Grouping.new(expression) if expression
       end
     end
 
+    def build_where_with_condition(opts, other = [])
+      if Sakuramochi::Condition.condition?(opts)
+        ast = Sakuramochi::Condition::Parser.new(opts.dup).parse
+        [collapse_conditions(ast, other)].compact
+      else
+        build_where_without_condition(opts, other)
+      end
+    end
   end
 end
